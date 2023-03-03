603e3d5c628e69f4be1e99a0ff227bbc1cd60270
Dustin Lott <dustin.lott93@gmail.com>
2023-03-02 19:55:58 -0800

    Modified BE route /signup to create a new user in the DB using users.service, removed unique trait for event_id

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get, Req, Post } from '@nestjs/common';
+import { Controller, Get, Req, Post, Inject } from '@nestjs/common';
 import { Request } from 'express';
-import { app } from 'firebase-admin';
 import { AppService } from './app.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
+import { UsersService } from './users/users.service';
+import { AnyKeys, AnyObject } from 'mongoose';
+import { User } from './users/schemas/user.schema';
+import { Interval } from './enums/enums.notifInterval';
+import { Preferences } from './enums/enums.eventPreferences';
+
+type BaseDoc<T> = AnyKeys<T> & AnyObject;
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly usersService: UsersService;
+  constructor(
+    // @Inject(UsersService)
+    usersService: UsersService,
+    private readonly appService: AppService,
+  ) {
+    this.usersService = usersService;
+  }
 
   @Get()
   getHello(): string {
@@ -18,8 +32,32 @@ export class AppController {
   }
 
   @Get('/signup')
-  getSignup(@Req() request: Request): string {
-    return 'Successfully Signed up ' + request['user']?.email + '!';
+  async getSignup(@Req() request: Request): Promise<string> {
+    // const emptyFriends = Array<Friend>();
+    // const emptyEvents = Array<Event>();
+    try {
+      const newUser: BaseDoc<User> = {
+        name: request['user']?.email,
+        email: request['user']?.email,
+        uid: request['user']?.uid,
+        profile_picture: ' ',
+        friends: null,
+        tags: [],
+        events: null,
+        social_medias: [],
+        settings: {
+          friend_hot_notifications: [Interval.OFF],
+          friend_cold_notifications: [Interval.OFF],
+          event_notifications: [Preferences.OFF],
+          dark_mode: false,
+        },
+      };
+      await this.usersService.create(newUser as CreateUserDto);
+      return 'Successfully Signed up ' + request['user']?.email + '!';
+    } catch (error) {
+      console.log(error);
+      return 'Failed to create user.';
+    }
   }
 
   //for testing purposes, will be removed/refactored later
