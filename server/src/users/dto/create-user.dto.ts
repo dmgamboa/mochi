8bfe1a65701e172e894f51e461670d44da0ece0f
Dustin Lott <78812567+DustinJoshua@users.noreply.github.com>
2023-03-02 21:12:37 -0800

    Modified BE route /signup to create a new user in the DB using users.service (#9)
    * Modified BE route /signup to create a new user in the DB using users.service, removed unique trait for event_id
    
    * added display_message to user schema and DTO, as well was placholder data when signing up

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -4,13 +4,14 @@ import { EventHistory } from '../schemas/eventHistory.schema';
 import { Settings } from '../schemas/settings.schema';
 
 export class CreateUserDto {
-    readonly name: string;
-    readonly email: string;
-    readonly uid: string;
-    readonly profile_picture: string;
-    readonly friends: Types.Array<Friend>;
-    readonly tags: string[];
-    readonly events: Types.Array<EventHistory>;
-    readonly social_medias: string[];
-    readonly settings: Settings;
+  readonly name: string;
+  readonly email: string;
+  readonly uid: string;
+  readonly profile_picture: string;
+  readonly display_message: string;
+  readonly friends: Types.Array<Friend>;
+  readonly tags: string[];
+  readonly events: Types.Array<EventHistory>;
+  readonly social_medias: string[];
+  readonly settings: Settings;
 }
--- server/src/users/schemas/user.schema.ts
+++ server/src/users/schemas/user.schema.ts
@@ -10,49 +10,52 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-    @Prop({required: true})
-    name: string;
+  @Prop({ required: true })
+  name: string;
 
-    @Prop({required: true, unique: true})
-    email: string;
+  @Prop({ required: true, unique: true })
+  email: string;
 
-    @Prop({required: true})
-    uid: string;
+  @Prop({ required: true })
+  uid: string;
 
-    @Prop({required: true})
-    profile_picture: string;
+  @Prop({ required: true })
+  profile_picture: string;
 
-    @Prop({
-        type: [FriendSchema],
-        //required: true,
-    })
-    friends: Types.Array<Friend>;
+  @Prop({ required: true })
+  display_message: string;
 
-    @Prop({
-        type: [String],
-        enum: Tag,
-        required: true,
-    })
-    tags: string[];
+  @Prop({
+    type: [FriendSchema],
+    //required: true,
+  })
+  friends: Types.Array<Friend>;
 
-     @Prop({
-        type: [EventHistorySchema],
-        //required: true,
-     })
-    events: Types.Array<EventHistory>;
+  @Prop({
+    type: [String],
+    enum: Tag,
+    required: true,
+  })
+  tags: string[];
 
-    @Prop({
-        type: [String],
-        enum: SocialMedia,
-        required: true,
-    })
-    social_medias: string[];
+  @Prop({
+    type: [EventHistorySchema],
+    //required: true,
+  })
+  events: Types.Array<EventHistory>;
 
-    @Prop({
-        type: SettingsSchema,
-        required: true,
-    })
-    settings: Settings;
+  @Prop({
+    type: [String],
+    enum: SocialMedia,
+    required: true,
+  })
+  social_medias: string[];
+
+  @Prop({
+    type: SettingsSchema,
+    required: true,
+  })
+  settings: Settings;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
